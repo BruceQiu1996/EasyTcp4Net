ed96b0fbf611ba685bf10332d1e3eb070bbef64c
qiuwei <bruce.qiu@meetfuture.com>
2024-07-29 15:15:45 +0800

    remove example

--- Example/FileTransfer/Models/FileSendWithRemoteChannelModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FileTransfer.Models
-{
-    public class FileSendWithRemoteChannelModel
-    {
-        public FileSendRecordModel FileSendRecordModel { get; set; }
-        public RemoteChannelModel RemoteChannelModel { get; set; }
-    }
-}
